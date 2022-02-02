0455758aebd88b84f60275a86d529ebf923c4df2
akuny <andykuny@gmail.com>
2022-02-02 07:54:07 -0500

    Fiddle with shellsort

--- lib/sorting/shellSort.ts
+++ lib/sorting/shellSort.ts
@@ -4,7 +4,16 @@ export function shellSort(collection: number[]): void {
     let n = collection.length;
     let h = 1;
 
-    while (h < n / Math.floor(3)) {
+    /**
+     * "By h-sorting for some large values of h, we can move
+     * entries in the array long distances and thus make it
+     * easier to h-sort for smaller values of h. Using such a
+     * procedure for any increment sequence of values of h that
+     * ends in 1 will produce a sorted array."
+     *
+     * https://algs4.cs.princeton.edu/21elementary/
+     */
+    while (h < Math.floor(n / 3)) {
         h = Math.floor(3 * h) + 1;
     }
 
--- test/shellSort.test.ts
+++ test/shellSort.test.ts
@@ -3,11 +3,14 @@ import { shellSort } from '../lib/sorting/shellSort';
 
 tap.test('shellSort', (t) => {
     t.test('sorts', (t) => {
-        let myArray = [9, 7, 5, 3, 1];
+        let myArray = [9, 7, 5, 3, 1, 120, 4, 5, 77, -4, -3, -50, 21, 600, 3];
 
         shellSort(myArray);
 
-        t.same(myArray, [1, 3, 5, 7, 9]);
+        t.same(
+            myArray,
+            [-50, -4, -3, 1, 3, 3, 4, 5, 5, 7, 9, 21, 77, 120, 600]
+        );
         t.end();
     });
 
